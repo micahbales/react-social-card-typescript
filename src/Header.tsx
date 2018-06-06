9bfdea29ef7b872da27808254e6fc26cd5830626
Micah Bales <micahbales@gmail.com>
2018-06-05 22:37:06 -0400

    Initial Commit

---
+++ src/Header.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+interface IProps {
+    name?: string;
+}
+
+const Header: React.SFC<IProps> = (props: IProps) => (
+    <h1>
+        Hello, {props.name}! Welcome to React and Typescript.
+    </h1>
+);
+
+Header.defaultProps = {
+    name: 'world',
+}
+
+export default Header;
