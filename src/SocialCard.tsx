4ef36ad5d5cd23c561ebba28fefd03e17d505787
Micah Bales <micahbales@gmail.com>
2018-06-10 22:02:12 -0400

    Add Text Body

--- src/SocialCard.tsx
+++ src/SocialCard.tsx
@@ -4,6 +4,7 @@ import './SocialCard.css';
 interface IProps {
     avatarImgUrl: string;
     name: string;
+    textBody: string;
     timestamp: string;
     username: string;
 }
@@ -12,21 +13,28 @@ class SocialCard extends React.Component<IProps> {
     public render() {
         return (
             <div className="social-card">
-                <div className="avatar-img">
-                    <a href={`http://www.twitter.com/${this.props.username}`}>
-                        <img src={this.props.avatarImgUrl} className="avatar-img__img" />
-                    </a>
+
+                <div className="top-row">
+                    <div className="avatar-img">
+                        <a href={`http://www.twitter.com/${this.props.username}`}>
+                            <img src={this.props.avatarImgUrl} className="avatar-img__img" />
+                        </a>
+                    </div>
+                    <div className="name">
+                        <p>{this.props.name}</p>
+                    </div>
+                    <div className="username">
+                        <a href={`http://www.twitter.com/${this.props.username}`}>
+                            <p>@{this.props.username}</p>
+                        </a>
+                    </div>
+                    <div className="timestamp">
+                        <p>{this.props.timestamp}</p>
+                    </div>
                 </div>
-                <div className="name">
-                    <p>{this.props.name}</p>
-                </div>
-                <div className="username">
-                    <a href={`http://www.twitter.com/${this.props.username}`}>
-                        <p>@{this.props.username}</p>
-                    </a>
-                </div>
-                <div className="timestamp">
-                    <p>{this.props.timestamp}</p>
+
+                <div className="text-body">
+                    <p>{this.props.textBody}</p>
                 </div>
             </div>
         )
